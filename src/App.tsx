16826e37d30438d3dcdf463d34958815599df529
skinnyK <skinnykdev@gmail.com>
2025-09-30 21:01:33 +0200

    set up routing and installed react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+};
+
 const App = () => {
-  return <h1 className="text-3xl text-primary-800">Tailwind is working</h1>;
+  return (
+    <Router basename="/portfolio">
+      {/* <ContextProvider> */}
+      {/* <Header /> */}
+      <div className="bg-primary-800 min-h-[90vh]">
+        <AppRoutes />
+      </div>
+      {/* <Footer /> */}
+      {/* </ContextProvider> */}
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="bg-lime-200">Home page component</h1>
+    </>
+  );
+};
+
+export default Home;
