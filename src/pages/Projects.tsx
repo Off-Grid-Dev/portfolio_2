88df3251f80950bba30eb27444749effb30ce10c
skinnyK <skinnykdev@gmail.com>
2025-10-03 18:43:14 +0200

    wrapped Projects page with Main component. will move to routes in app.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,10 @@
+import Main from "../layout/Main";
+
 const ProjectsPage = () => {
   return (
-    <>
+    <Main>
       <h1 className="text-3xl text-lime-300 text-shadow-slate-200">Projects</h1>
-    </>
+    </Main>
   );
 };
 
